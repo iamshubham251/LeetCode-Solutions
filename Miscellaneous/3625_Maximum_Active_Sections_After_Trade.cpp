ee3a50b9693b6f780e49bd255b7d1b5888511ed5
Shubham pal <palshubahm251@gmail.com>
2026-07-21 10:20:16 +0530

    Create 3625_Maximum_Active_Sections_After_Trade.cpp

---
+++ Miscellaneous/3625_Maximum_Active_Sections_After_Trade.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int cnt1 = 0;
+        for (char c : s)
+            if (c == '1') cnt1++;
+        string t = "1" + s + "1";
+        vector<pair<char, int>> runs;
+        for (char c : t) {
+            if (runs.empty() || runs.back().first != c)
+                runs.push_back({c, 1});
+            else
+                runs.back().second++;
+        }
+        int bestGain = 0;
+        for (int i = 1; i + 1 < runs.size(); i++) {
+            if (runs[i].first == '1' &&
+                runs[i - 1].first == '0' &&
+                runs[i + 1].first == '0') {
+                bestGain = max(bestGain,
+                               runs[i - 1].second + runs[i + 1].second);
+            }
+        }
+        return cnt1 + bestGain;
+    }
+};
