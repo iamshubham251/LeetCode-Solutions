9ca2376e0e5b51df70c039047549c44478d09565
Shubham pal <palshubahm251@gmail.com>
2026-07-23 10:44:03 +0530

    Create 3513_Number_of_Unique_XOR_Triplets_I.cpp

---
+++ Miscellaneous/3513_Number_of_Unique_XOR_Triplets_I.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int k = 31 - __builtin_clz(n);
+        return 1 << (k + 1);
+    }
+};
