0daed6ac01460a78982b5dc4e24d0872794ae82d
Shubham pal <palshubahm251@gmail.com>
2026-08-05 22:46:17 +0530

    Create 3310_Remove_Methods_From_Project.cpp

---
+++ Graphs/3310_Remove_Methods_From_Project.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+
+        for (auto &x : invocations) {
+            adj[x[0]].push_back(x[1]);
+        }
+        vector<bool> suspicious(n, false);
+        queue<int> q;
+
+        q.push(k);
+        suspicious[k] = true;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : adj[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+        for (auto &x : invocations) {
+            int u = x[0];
+            int v = x[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
